1f2783a5c56f9b8aa7668c3e52723ddca2220d0d
c0dehard <47797039+c0dehard@users.noreply.github.com>
2019-11-20 23:19:52 +0100

    Add files via upload

---
+++ LowerUpper_UnderlinedInputConvertWithHistoryList.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+verlauf,i,programAn = [],0,True
+
+def abfrage(text,eingabe):
+    if eingabe.casefold() == "g":print(text.upper())
+    elif eingabe.casefold() == "k":print(text.lower());global i;i = i + 1
+    verlauf.append(f"{i}) {text}");weiter = input('Weiter? <J/N>')
+    if weiter == "n".casefold():programAn=False
+    
+while programAn:
+    print("Im Verlauf: ",verlauf);abfrage(input('Gib ein Wort ein\n> '),input('Alles G\u0332roß, Alles K\u0332lein <G/K>\n> '))
