e49bde6ed4a7ef1bcc0b405741ca91e19060b772
c0dehard <47797039+c0dehard@users.noreply.github.com>
2019-11-22 09:37:58 +0100

    Update LowerUpper_UnderlinedInputConvertWithHistoryList.py

--- LowerUpper_UnderlinedInputConvertWithHistoryList.py
+++ LowerUpper_UnderlinedInputConvertWithHistoryList.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 verlauf,i,programAn = [],0,True
 
+def reverse(s): 
+  str = "" 
+  for i in s: 
+    str = i + str
+  return str
+
 def abfrage(text,eingabe):
     if eingabe.casefold() == "g":print(text.upper())
-    elif eingabe.casefold() == "k":print(text.lower());global i;i = i + 1
+    elif eingabe.casefold() == "k":print(text.lower())
+    elif eingabe.casefold()=="u":print(reverse(text))
+    global i;i = i + 1
     verlauf.append(f"{i}) {text}");weiter = input('Weiter? <J/N>')
     if weiter == "n".casefold():programAn=False
     
 while programAn:
-    print("Im Verlauf: ",verlauf);abfrage(input('Gib ein Wort ein\n> '),input('Alles g\u0332roß, Alles k\u0332lein <G/K>\n> '))
+    print("Im Verlauf: ",verlauf);abfrage(input('Gib ein Wort ein\n> '),input('Alles g\u0332roß, Alles k\u0332lein oder u\u0332mgekehrt <G/KU>\n> '))
