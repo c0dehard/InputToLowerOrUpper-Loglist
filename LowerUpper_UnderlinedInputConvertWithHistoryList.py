02526356c96b24a2ca54c98b553cb7ae3d25dede
c0dehard <47797039+c0dehard@users.noreply.github.com>
2019-12-01 20:33:44 +0100

    Neue elifs

--- LowerUpper_UnderlinedInputConvertWithHistoryList.py
+++ LowerUpper_UnderlinedInputConvertWithHistoryList.py
@@ -12,9 +12,12 @@ def abfrage(text,eingabe):
     if eingabe.casefold() == "g":print(text.upper())
     elif eingabe.casefold() == "k":print(text.lower())
     elif eingabe.casefold()=="u":print(reverse(text))
+    elif eingabe.casefold() == "l":print(text.strip())
+    elif eingabe.casefold() == "a":print(str(text.replace(input('Tausche: '),input('mit:'))))
     global i;i = i + 1
     verlauf.append(f"{i}) {text}");weiter = input('Weiter? <J/N>')
     if weiter == "n".casefold():programAn=False
     
 while programAn:
-    print("Im Verlauf: ",verlauf);abfrage(input('Gib ein Wort ein\n> '),input('Alles g\u0332roß, Alles k\u0332lein oder u\u0332mgekehrt <G/KU>\n> '))
+    print("Im Verlauf: ",verlauf);abfrage(input('Gib ein Wort ein\n> '),input('''
+Alles g\u0332roß, Alles k\u0332lein, Ohne L\u0332eerzeichen oder A\u0332ustauschen< g/k/L/A>\n> '''))
